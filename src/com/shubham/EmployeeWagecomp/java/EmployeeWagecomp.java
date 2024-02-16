211c077bccf243836938b32c7255541932233f57
shubhamgan <shubhamgangurde20003@gmail.com>
2024-02-16 03:03:28 -0800

    ADDED : Employee wages  compilation

---
+++ src/com/shubham/EmployeeWagecomp/java/EmployeeWagecomp.java
@@ -0,0 +1,11 @@
+package com.shubham.EmployeeWagecomp.java;
+
+import java.util.Random;
+
+public class EmployeeWagecomp {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Employee Wage computation program");
+	}	
+}
