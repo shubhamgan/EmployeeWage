843677435bd84e000a80657d566ea4f64088e494
shubhamgan <shubhamgangurde20003@gmail.com>
2024-02-16 03:13:41 -0800

    [MODIFIED]: shubham - UC-1- completed for Employee Attendence

--- src/com/shubham/EmployeeWagecomp/java/EmployeeWagecomp.java
+++ src/com/shubham/EmployeeWagecomp/java/EmployeeWagecomp.java
@@ -6,6 +6,19 @@ public class EmployeeWagecomp {
 
 	public static void main(String[] args) {
 
+		int attendance = 0;
 		System.out.println("Welcome to Employee Wage computation program");
+		
+		Random random = new Random();
+		attendance = random.nextInt(9) % 2;
+		
+		if(attendance == 0)
+		{
+			System.out.println("Employee is Absend");
+		}
+		else
+		{
+			System.out.println("Employee is present");
+		}
 	}	
 }
