938eaca218f9e73b1696ce7b9cc004277bc828bc
shubhamgan <shubhamgangurde20003@gmail.com>
2024-02-16 20:43:49 -0800

    [MODIFIED]:shubham calculate daily Employee wage

--- src/com/shubham/EmployeeWagecomp/java/EmployeeWagecomp.java
+++ src/com/shubham/EmployeeWagecomp/java/EmployeeWagecomp.java
@@ -7,6 +7,9 @@ public class EmployeeWagecomp {
 	public static void main(String[] args) {
 
 		int attendance = 0;
+		int dailywage =0;
+		int WAGE_PER_HR = 20;
+		int FULL_DAY_HR = 8;
 		System.out.println("Welcome to Employee Wage computation program");
 		
 		Random random = new Random();
@@ -19,6 +22,8 @@ public class EmployeeWagecomp {
 		else
 		{
 			System.out.println("Employee is present");
+			dailywage = FULL_DAY_HR * WAGE_PER_HR;
+			System.out.println("Dailywage="+dailywage);
 		}
 	}	
 }
