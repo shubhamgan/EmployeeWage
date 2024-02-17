f8479e0d7cd8df35279dd9617e149a7b90af7079
shubhamgan <shubhamgangurde20003@gmail.com>
2024-02-16 23:42:43 -0800

    [MODIFIED]:shubham Refactored if-else to Switch case

--- src/com/shubham/EmployeeWagecomp/java/EmployeeWagecomp.java
+++ src/com/shubham/EmployeeWagecomp/java/EmployeeWagecomp.java
@@ -7,37 +7,38 @@ public class EmployeeWagecomp {
 	public static void main(String[] args) {
 
 		int attendance = 0;
-		int dailywage =0;
+		int dailywage = 0;
 		int employee_type = 0;
-		int WAGE_PER_HR = 20;
-		int FULL_TIME_HR = 8;
-		int PART_TIME_HR = 4;
+	    final int WAGE_PER_HR = 20;
+		final int FULL_TIME_HR = 8;
+		final int PART_TIME_HR = 4;
 		System.out.println("Welcome to Employee Wage computation program");
 		
 		Random random = new Random();
 		attendance = random.nextInt(9) % 2;
-		
-		if(attendance == 0)
+		switch(attendance)
 		{
+		case 0:
 			System.out.println("Employee is Absend");
-		}
-		else
-		{
+			break;
+		case 1:
 			System.out.println("Employee is present");
 			employee_type = random.nextInt(9) % 2;
-		   if(employee_type == 0)
-		   {
-			   dailywage = PART_TIME_HR * WAGE_PER_HR;
-			   System.out.println("Employee is part_time");
-				
-			}
-		   else
-		   {
-			 dailywage = FULL_TIME_HR * WAGE_PER_HR;
-			 System.out.println("Employee is full_time");
-			
-		   }
-		   System.out.println("dailywage="+dailywage);
-		}
-	}	
-}
+		    switch(employee_type)
+		      {
+		      case 0:
+		    	  dailywage = PART_TIME_HR * WAGE_PER_HR;
+		    	  System.out.println("Employee is part_time");
+		    	  break;
+		      case 1:
+		    	  dailywage = FULL_TIME_HR * WAGE_PER_HR;
+				  System.out.println("Employee is full_time");	
+				  break;
+			   }
+		    System.out.println("dailywage="+dailywage);
+		   break;
+	   	}
+		   
+		
+	   }	
+	}
